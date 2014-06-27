6c3f78101a62b6ab6df4ad75a138f1f6075f0058
Bibhas <iambibhas@gmail.com>
2014-06-27 22:59:05 +0530

    also searching for title cased query, turned tabs to spaces

--- src/geohack/apps/awosm/MapFragment.java
+++ src/geohack/apps/awosm/MapFragment.java
@@ -1,5 +1,11 @@
 package geohack.apps.awosm;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 import org.apache.http.Header;
@@ -28,97 +34,129 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class MapFragment extends Fragment {
-	private MyLocationNewOverlay mLocationOverlay;
+    private MyLocationNewOverlay mLocationOverlay;
     private CompassOverlay mCompassOverlay;
-    
-	public MapFragment() {
-	}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_map, container,
-				false);
-		
-		final Context context = rootView.getContext();
-		GeoPoint startPoint = new GeoPoint(22.4109, 88.6216);
-		
-		final MapView mapView = (MapView) rootView.findViewById(R.id.mapview);
+    public MapFragment() {
+    }
+
+    private void cacheLocation(Context context, GeoPoint center) {
+        String FILENAME = "awosm.lastpos";
+
+        FileOutputStream fos;
+        try {
+            fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(center.toDoubleString().getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private String readCachedLocationString(Context context) {
+        String FILENAME = "awosm.lastpos";
+        StringBuffer datax = new StringBuffer("");
+        try {
+            FileInputStream fIn = context.openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fIn);
+            BufferedReader buffreader = new BufferedReader(isr);
+
+            String readString = buffreader.readLine();
+            while (readString != null) {
+                datax.append(readString);
+                readString = buffreader.readLine();
+            }
+
+            isr.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return datax.toString();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        final Context context = rootView.getContext();
+        GeoPoint startPoint = new GeoPoint(22.4109, 88.6216);
+
+        final MapView mapView = (MapView) rootView.findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
-        
+
         mapView.getController().setZoom(16);
         mapView.getController().setCenter(startPoint);
-        
+
         GpsMyLocationProvider imlp = new GpsMyLocationProvider(context);
         imlp.setLocationUpdateMinDistance(10);
         imlp.setLocationUpdateMinTime(5000);
-        
-        mLocationOverlay = new MyLocationNewOverlay(context, imlp, mapView);
-    	mLocationOverlay.enableMyLocation();
-    	mapView.getOverlays().add(mLocationOverlay);
-    	
-    	/*
-    	Handles the search bar
-    	*/
-    	final SearchView osmSearchView = (SearchView) rootView.findViewById(R.id.OSMSearchView);
-    	osmSearchView.setOnQueryTextListener(new OnQueryTextListener() {
-			
-			@Override
-			public boolean onQueryTextSubmit(String query) {
-				// TODO Auto-generated method stub
-				Log.d("Search", query);
-				Projection bboxProjection = mapView.getProjection();
-		    	
-				OverpassApiWrapper overpass = new OverpassApiWrapper();
-				overpass.getResults(
-					query,
-					bboxProjection,
-					new AsyncHttpResponseHandler() {
-						
-						@Override
-						public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-							// TODO Auto-generated method stub
-							String jsonResponse = new String(response);
-							Log.d("API", jsonResponse);
-						}
-						
-						@Override
-						public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-							// TODO Auto-generated method stub
-							
-							String error = Arrays.toString(errorResponse);
-							Log.d("API Error", String.valueOf(statusCode));
-							Log.d("API Error", error);
-						}
-					}
-				);
-				return false;
-			}
-			
-			@Override
-			public boolean onQueryTextChange(String newText) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-		});
-    	
-		return rootView;
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-	    mLocationOverlay.enableMyLocation();
-	    mLocationOverlay.enableFollowLocation();
-	}
-	
 
-	@Override
-	public void onPause() {
-		super.onPause();
-	    mLocationOverlay.disableMyLocation();
-	    mLocationOverlay.disableFollowLocation();
-	}
-	
+        mLocationOverlay = new MyLocationNewOverlay(context, imlp, mapView);
+        mLocationOverlay.enableMyLocation();
+        mapView.getOverlays().add(mLocationOverlay);
+
+        /*
+         * Handles the search bar
+         */
+        final SearchView osmSearchView = (SearchView) rootView.findViewById(R.id.OSMSearchView);
+        osmSearchView.setOnQueryTextListener(new OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // TODO Auto-generated method stub
+                Log.d("Search", query);
+                Projection bboxProjection = mapView.getProjection();
+
+                OverpassApiWrapper overpass = new OverpassApiWrapper();
+                overpass.getResults(query, bboxProjection, new AsyncHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                        // TODO Auto-generated method stub
+                        String jsonResponse = new String(response);
+                        Log.d("API", jsonResponse);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse,
+                            Throwable e) {
+                        // TODO Auto-generated method stub
+
+                        String error = Arrays.toString(errorResponse);
+                        Log.d("API Error", String.valueOf(statusCode));
+                        Log.d("API Error", error);
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // TODO Auto-generated method stub
+                return false;
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mLocationOverlay.enableMyLocation();
+        mLocationOverlay.enableFollowLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mLocationOverlay.disableMyLocation();
+        mLocationOverlay.disableFollowLocation();
+    }
+
 }
--- src/geohack/apps/awosm/OverpassApiWrapper.java
+++ src/geohack/apps/awosm/OverpassApiWrapper.java
@@ -8,11 +8,11 @@ import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.views.Projection;
 import org.xmlpull.v1.XmlSerializer;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
 
 import com.loopj.android.http.*;
-
 
 //<osm-script output="json" timeout="25">
 //  <!-- gather results -->
@@ -33,108 +33,126 @@ import com.loopj.android.http.*;
 //  <print mode="skeleton" order="quadtile"/>
 //</osm-script>
 
-
 public class OverpassApiWrapper {
-	public String baseUrl = "http://overpass-api.de/api/interpreter";
-	public String outputFormat = "json";
-	public Integer timeout = 25;
-	
-	public String getXmlFromQuery(String query, Projection bboxProjection) {
-		XmlSerializer xmlSerializer = Xml.newSerializer();
-	    StringWriter writer = new StringWriter();
-	    
-	    String regexQuery = "("
-	    	+ query
-	    	+ "|" 
-	    	+ query.toLowerCase(Locale.getDefault()) 
-	    	+ "|" 
-	    	+ query.toUpperCase(Locale.getDefault())
-	    	+ ")";
-	    String amenityQuery = query.toLowerCase(Locale.getDefault()).replace(" ", "_");
-	    
-	    try {
-	    	xmlSerializer.setOutput(writer);
-			xmlSerializer.startDocument("UTF-8", true);
-			
-			xmlSerializer.startTag("", "osm-script");
-			xmlSerializer.attribute("", "output", this.outputFormat);
-			xmlSerializer.attribute("", "timeout", this.timeout.toString());
-			
-			xmlSerializer.startTag("", "union");
-			
-			this.addNodeToQuery(xmlSerializer, true, "name", regexQuery, bboxProjection);
-			this.addNodeToQuery(xmlSerializer, true, "amenity", amenityQuery, bboxProjection);
-			
-			xmlSerializer.endTag("", "union");
+    public String baseUrl = "http://overpass-api.de/api/interpreter";
+    public String outputFormat = "xml";
+    public Integer timeout = 25;
 
-			xmlSerializer.startTag("", "print");
-			xmlSerializer.attribute("", "mode", "body");
-			xmlSerializer.endTag("", "print");
+    public String getXmlFromQuery(String query, Projection bboxProjection) {
+        XmlSerializer xmlSerializer = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
 
-			xmlSerializer.startTag("", "recurse");
-			xmlSerializer.attribute("", "type", "down");
-			xmlSerializer.endTag("", "recurse");
+        String regexQuery = "("
+                + query + "|"
+                + query.toLowerCase(Locale.getDefault()) + "|"
+                + query.toUpperCase(Locale.getDefault()) + "|"
+                + this.toTitleCase(query) + ")";
+        String amenityQuery = query.toLowerCase(Locale.getDefault()).replace(" ", "_");
 
-			xmlSerializer.startTag("", "print");
-			xmlSerializer.attribute("", "mode", "skeleton");
-			xmlSerializer.attribute("", "order", "quadtile");
-			xmlSerializer.endTag("", "print");
-			
-			xmlSerializer.endTag("", "osm-script");
-			
-			xmlSerializer.endDocument();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	    
-	    return writer.toString();
-	}
-	
-	public void getResults(String query, Projection bboxProjection, AsyncHttpResponseHandler responseHandler) {
-		String xml = this.getXmlFromQuery(query, bboxProjection);
-		Log.d("OverPass XML", xml);
-		
-		AsyncHttpClient client = new AsyncHttpClient();
-		RequestParams params = new RequestParams();
-		params.put("data", xml);
-		client.post(this.baseUrl, params, responseHandler);
-	}
-	
-	private void addNodeToQuery(XmlSerializer xmlSerializer, Boolean regex, String key, String value, Projection bboxProjection) {
-		try {
-			String[] types = {"node", "way", "relation"};
-			for (String type : types) {
-				xmlSerializer.startTag("", "query");
-				xmlSerializer.attribute("", "type", type);
-				xmlSerializer.startTag("", "has-kv");
-				xmlSerializer.attribute("", "k", key);
-				
-				if (regex) {
-					xmlSerializer.attribute("", "regv", value);
-				} else {
-					xmlSerializer.attribute("", "v", value);
-				}
-				
-				xmlSerializer.endTag("", "has-kv");
-				xmlSerializer.startTag("", "bbox-query");
-				xmlSerializer.attribute("", "s", String.valueOf(bboxProjection.getSouthWest().getLatitude()));
-				xmlSerializer.attribute("", "w", String.valueOf(bboxProjection.getSouthWest().getLongitude()));
-				xmlSerializer.attribute("", "n", String.valueOf(bboxProjection.getNorthEast().getLatitude()));
-				xmlSerializer.attribute("", "e", String.valueOf(bboxProjection.getNorthEast().getLongitude()));
-				xmlSerializer.endTag("", "bbox-query");
-				xmlSerializer.endTag("", "query");
-			}
-			
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        try {
+            xmlSerializer.setOutput(writer);
+            xmlSerializer.startDocument("UTF-8", true);
+
+            xmlSerializer.startTag("", "osm-script");
+            xmlSerializer.attribute("", "output", this.outputFormat);
+            xmlSerializer.attribute("", "timeout", this.timeout.toString());
+
+            xmlSerializer.startTag("", "union");
+
+            this.addNodeToQuery(xmlSerializer, true, "name", regexQuery, bboxProjection);
+            this.addNodeToQuery(xmlSerializer, true, "amenity", amenityQuery, bboxProjection);
+
+            xmlSerializer.endTag("", "union");
+
+            xmlSerializer.startTag("", "print");
+            xmlSerializer.attribute("", "mode", "body");
+            xmlSerializer.endTag("", "print");
+
+            xmlSerializer.startTag("", "recurse");
+            xmlSerializer.attribute("", "type", "down");
+            xmlSerializer.endTag("", "recurse");
+
+            xmlSerializer.startTag("", "print");
+            xmlSerializer.attribute("", "mode", "skeleton");
+            xmlSerializer.attribute("", "order", "quadtile");
+            xmlSerializer.endTag("", "print");
+
+            xmlSerializer.endTag("", "osm-script");
+
+            xmlSerializer.endDocument();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return writer.toString();
+    }
+
+    public void getResults(String query, Projection bboxProjection,
+            AsyncHttpResponseHandler responseHandler) {
+        String xml = this.getXmlFromQuery(query, bboxProjection);
+        Log.d("OverPass XML", xml);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("data", xml);
+        client.post(this.baseUrl, params, responseHandler);
+    }
+
+    private void addNodeToQuery(XmlSerializer xmlSerializer, Boolean regex, String key,
+            String value, Projection bboxProjection) {
+        try {
+            String[] types = { "node", "way", "relation" };
+            for (String type : types) {
+                xmlSerializer.startTag("", "query");
+                xmlSerializer.attribute("", "type", type);
+                xmlSerializer.startTag("", "has-kv");
+                xmlSerializer.attribute("", "k", key);
+
+                if (regex) {
+                    xmlSerializer.attribute("", "regv", value);
+                } else {
+                    xmlSerializer.attribute("", "v", value);
+                }
+
+                xmlSerializer.endTag("", "has-kv");
+                xmlSerializer.startTag("", "bbox-query");
+                xmlSerializer.attribute("", "s",
+                        String.valueOf(bboxProjection.getSouthWest().getLatitude()));
+                xmlSerializer.attribute("", "w",
+                        String.valueOf(bboxProjection.getSouthWest().getLongitude()));
+                xmlSerializer.attribute("", "n",
+                        String.valueOf(bboxProjection.getNorthEast().getLatitude()));
+                xmlSerializer.attribute("", "e",
+                        String.valueOf(bboxProjection.getNorthEast().getLongitude()));
+                xmlSerializer.endTag("", "bbox-query");
+                xmlSerializer.endTag("", "query");
+            }
+
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public String toTitleCase(String str) {
+        final String[] arr = TextUtils.split(str, " ");
+        final int len = arr.length;
+
+        for (int i = 0; i < len; i++) {
+            final String s = arr[i];
+            final String s0 = "" + s.toUpperCase().charAt(0);
+            final String s1 = s.toLowerCase().substring(1, s.length());
+            arr[i] = s0 + s1;
+        }
+
+        str = TextUtils.join(" ", arr);
+        return str;
+    }
 }
